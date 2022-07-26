ab5de3505bfb0f0aa17b1114d8222a3bd52d13bc
Nikhil Sharma <50795450+imnikhilsharma@users.noreply.github.com>
2022-07-26 15:46:26 +0530

    Create tracker.js

---
+++ tracker.js
@@ -0,0 +1,45 @@
+// submit button.
+let submitButton = document.getElementsByClassName('action submit primary')[0];
+
+// added click event listner to the button.
+submitButton.addEventListener("click", trackEvents);
+
+function trackEvents() {
+    let orderId = document.getElementById('oar-order-id');
+    let billingLastName = document.getElementById('oar-billing-lastname');
+   
+    let findOrderByType;
+    let findOrderByValue;
+    let emailZipEmementId;
+
+    // fetching selected Find Order By
+    let selectedFindOrderBy = document.getElementById('quick-search-type-id').value;
+   
+    if(selectedFindOrderBy === 'email'){
+        findOrderByType = 'email';
+        emailZipElementId = 'oar_email';
+    }else{
+        findOrderByType = 'zip';
+        emailZipElementId = 'oar_zip';
+    }
+
+    // fetching email/zip value as per above selection.
+    let emailZipValue = document.getElementById(emailZipEmementId).value;
+
+   
+    // event fire.
+    window.dataLayer.push({
+        "event": "ContinueClicked",
+        "eventLabel": "OrderAndReturns",
+        "orderId": orderId,
+        "billingLastName" : billingLastName,
+        "findOrderByType" : findOrderByType,
+        "emailZipValue" : emailZipValue,
+        "eventAction": "Continue"
+    })
+   
+   
+
+
+   
+}
